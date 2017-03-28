566db53b8bdb64b68bbc3f89aae5201b77a8efc7
Ann Wolff <annwolffe@gmail.com>
2017-03-28 17:45:15 -0600

    Add seed file for users and assignments
    This updates the seed file to create users and assign some other users
    to them to be described.
    
    [#142649211]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 class Seed
 
   def self.start
+    clear_database
     seed = Seed.new
     seed.generate_adjectives
+    seed.generate_users
+    seed.generate_assignments
+  end
+
+  def self.clear_database
+    User.delete_all
+    Assignment.delete_all
+    Adjective.delete_all
+    Description.delete_all
   end
 
   def generate_adjectives
@@ -16,6 +26,20 @@ class Seed
     shy silly spontaneous sympathetic tense trustworthy warm wise witty)
     
     adjectives.each { |adj| Adjective.create!(name: adj)}
+  end
+
+  def generate_users
+    150.times do 
+      User.create!(name: Faker::GameOfThrones.character)
+    end
+  end
+
+  def generate_assignments
+    User.all.each do |user|
+      5.times do
+        user.assignments.create!(assigned: User.all.sample)
+      end
+    end
   end
 
 end
