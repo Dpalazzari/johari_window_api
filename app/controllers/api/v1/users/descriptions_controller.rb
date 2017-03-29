adb4d4e7b8a43df5549cbb53fcc36a94170931a0
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-29 14:14:46 -0600

    Pass post to user descriptions endpoint request test

--- app/controllers/api/v1/users/descriptions_controller.rb
+++ app/controllers/api/v1/users/descriptions_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::Users::DescriptionsController < Api::V1::BaseController
+  
   def create
+    describee    = User.find(params[:id])
     descriptions = JSON.parse(request.body.read)
-    binding.pry
-    user = User.find(params[:id])
+    describer    = User.find(descriptions['describer_id'])
+    assignment   = Assignment.where("assignee_id = ? AND assigned_id = ?", describer.id, describee.id).first
+    descriptions['johari'].each do |joh|
+      adj = Adjective.find_by(name: joh)
+      describee.descriptions.create(describer_id: describer.id, adjective_id: adj.id)
+    end
+    assignment.update(completed?: true)
+    assignment.save
+    render status: 202
   end
+
 end
--- spec/requests/api/v1/users/user_descriptions_spec.rb
+++ spec/requests/api/v1/users/user_descriptions_spec.rb
@@ -2,22 +2,25 @@ require 'rails_helper'
 
 describe 'Users Descriptions Endpoint', :type => :request do
   it "post /api/v1/users/:id/descriptions" do
+    create(:adjective, name: 'shy')
+    create(:adjective, name: 'religious')
+    create(:adjective, name: 'cool')
     user1, user2 = create_list(:user, 2)
-    create(:assignment, assignee: user1, assigned: user2)
+    assignment = create(:assignment, assignee: user1, assigned: user2)
     
     expect(user2.descriptions.count).to eq(0)
 
     adjectives = ['shy', 'religious', 'cool']
     headers = { 'CONTENT_TYPE' => 'application/json' }
-    json = {:johari => adjectives }
+    json = {:johari => adjectives, :describer_id => user1.id }
 
     post "/api/v1/users/#{user2.id}/descriptions", json.to_json, headers
-        
-    expect(response).to be_success
-
-    raw_status = JSON.parse(response.body)
+    assignment.reload
     
-    expect(raw_status).to be(202)
-    expect(user2.descriptions.count).to eq(1)
+    expect(response).to be_success
+    
+    expect(response.status).to be(202)
+    expect(user2.descriptions.count).to eq(3)
+    expect(assignment.completed?).to be_truthy
   end
 end
