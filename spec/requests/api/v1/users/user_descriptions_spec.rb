b4917af16adf198e31d39a2533091b5672273136
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-29 11:30:02 -0600

    Request.body now gets proper information in proper format

--- spec/requests/api/v1/users/user_descriptions_spec.rb
+++ spec/requests/api/v1/users/user_descriptions_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-describe 'Users Descriptions Endpoint' do
+describe 'Users Descriptions Endpoint', :type => :request do
   it "post /api/v1/users/:id/descriptions" do
     user1, user2 = create_list(:user, 2)
     create(:assignment, assignee: user1, assigned: user2)
     
     expect(user2.descriptions.count).to eq(0)
-    adjectives = ["shy", "religous", "cool"]
-    # conn = Faraday.new(:url => ) do |faraday|
-    #   faraday.adapter Faraday.default_adapter
-    # end
 
-    post "/api/v1/users/#{user2.id}/descriptions", { :format => :json, :description => adjectives }
-    
+    adjectives = ['shy', 'religious', 'cool']
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    json = {:johari => adjectives }
+
+    post "/api/v1/users/#{user2.id}/descriptions", json.to_json, headers
+        
     expect(response).to be_success
 
     raw_status = JSON.parse(response.body)
