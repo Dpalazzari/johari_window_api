f87c6920cb704b84271c55fbfbadde16996a4663
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-29 11:30:02 -0600

    Request.body now gets proper information in proper format

--- app/controllers/api/v1/users/descriptions_controller.rb
+++ app/controllers/api/v1/users/descriptions_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Users::DescriptionsController < Api::V1::BaseController
   def create
+    descriptions = JSON.parse(request.body.read)
     binding.pry
     user = User.find(params[:id])
   end
