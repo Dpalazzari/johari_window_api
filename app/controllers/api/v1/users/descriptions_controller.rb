d64d60327f7a224e7ee4b21150a6022fa1a9e525
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-30 14:27:36 -0600

    Change name of valid? method to invalid?

--- app/controllers/api/v1/users/descriptions_controller.rb
+++ app/controllers/api/v1/users/descriptions_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::Users::DescriptionsController < Api::V1::BaseController
     describee    = User.find(params[:id])
     descriptions = JSON.parse(request.body.read)
     describer    = User.find(descriptions['describer_id'])
-    render status: 402 and return if Assignment.valid?(describee.id, describer.id)
+    render status: 402 and return if Assignment.invalid?(describee.id, describer.id)
     describee.create_descriptions(descriptions['johari'], describer)
     if describee.all_descriptions_completed?(describer.id, descriptions['johari'])
       Assignment.complete(describee.id, describer.id)
