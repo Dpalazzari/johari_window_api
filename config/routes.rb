0c802ff5ac4ed0afa5ad7af97493dc3bde01e21b
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-30 13:11:54 -0600

    Add user show endpoint and test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get '/:id/assignments', to: 'assignments#index'
         post '/:id/descriptions', to: 'descriptions#create'
         get '/:id/descriptions', to: 'descriptions#index'
+        get '/:id', to: 'users#show'
       end
     end
   end
