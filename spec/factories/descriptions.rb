b314611fab03ab3c52a4b9e442e607cbd2954543
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-04-01 17:25:46 -0600

    Fill in description factory

--- spec/factories/descriptions.rb
+++ spec/factories/descriptions.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :description do
-    
+    association :describee, factory: :user
+    association :describer, factory: :user
+    association :adjective, factory: :adjective
   end
 end
