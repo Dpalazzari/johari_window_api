e66fe95c0c0cfec91b099cdbc0f5a236e18cae1c
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-04-01 18:57:07 -0600

    Refactor johari
    -Remove johari instance variable that is only called from within one
    method
    -Remove unnecessary selfs for method calls
    -Change methods to not use instance variables as arguments
    -Change map enumerable to find_all when appropriate

--- app/models/johari.rb
+++ app/models/johari.rb
@@ -1,45 +1,45 @@
 class Johari
-  attr_reader :user, :self_described, :peer_described, :johari
+  attr_reader :user, :self_described, :peer_described
 
   def initialize(user)
     @user = user
     @self_described = Description.find_adjectives(user.descriptions.where("describer_id = ?", user.id)) 
     @peer_described = Description.find_adjectives(user.descriptions.where.not("describer_id = ?", user.id)) 
-    @johari = Hash.new(0)
   end
 
   def format_descriptions
-    @johari["arena"] = self.arena(self_described, peer_described)
-    @johari["facade"] = self.facade(self_described, peer_described)
-    @johari["blind-spot"] = self.blind_spot(self_described, peer_described)
-    @johari["unknown"] = self.unknown
+    johari = Hash.new(0)
+    johari["arena"] = arena
+    johari["facade"] = facade
+    johari["blind-spot"] = blind_spot
+    johari["unknown"] = unknown
     johari
   end
 
-  def arena(self_described, peer_described)
-    adjectives = self_described.map do |adjective|
-      adjective if peer_described.include?(adjective)
+  def arena
+    adjectives = self_described.find_all do |adjective|
+      peer_described.include?(adjective)
     end.compact
     create_window(adjectives)
   end
  
-  def facade(self_described, peer_described)
-    adjectives = self_described.map do |adjective|
-      adjective if !peer_described.include?(adjective)
+  def facade
+    adjectives = self_described.find_all do |adjective|
+      !peer_described.include?(adjective)
     end.compact
     create_window(adjectives)
   end
   
-  def blind_spot(self_described, peer_described)
-    adjectives = peer_described.map do |adjective|
-      adjective if !self_described.include?(adjective)
+  def blind_spot
+    adjectives = peer_described.find_all do |adjective|
+      !self_described.include?(adjective)
     end.compact
     create_window(adjectives)
   end
   
   def unknown
-    unknown = Adjective.all.map do |adj|
-      adj if !user.adjectives.include?(adj)
+    unknown = Adjective.all.find_all do |adj|
+      !user.adjectives.include?(adj)
     end.compact
     create_window(unknown)
   end
