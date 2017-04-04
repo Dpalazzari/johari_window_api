957f025ffc1d9ba2ab5ba313f4fb2a006ce2f322
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-04-03 19:35:19 -0600

    Move cors middleware config

--- config/application.rb
+++ config/application.rb
@@ -26,13 +26,6 @@ module JohariWindowApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
     config.api_only = true
   end
 end
