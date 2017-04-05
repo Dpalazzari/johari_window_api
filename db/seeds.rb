a987900680d63ccfb4c257a8b5aa73583908b98c
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-05 14:47:09 -0600

    Update adjectives lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,17 @@ class Seed
   end
 
   def generate_adjectives
-    adjectives = %w(able accepting adaptable bold brave calm caring 
-    cheerful clever complex confident dependable dignified empathetic 
-    energetic extroverted friendly giving happy helpful idealistic 
-    independent ingenious intelligent introverted kind knowledgeable 
-    logical loving mature modest nervous observant organized patient 
-    powerful proud quiet reflective relaxed religious responsive 
-    searching self-assertive self-conscious sensible sentimental 
-    shy silly spontaneous sympathetic tense trustworthy warm wise witty)
+    adjectives = %w(able accepting accountable adaptable aggressive 
+    assertive available bold brave calm caring cheerful clever collaborative
+    communicative competitive complex confident dependable dignified discouraged 
+    disingenuous disorganized distracted driven egotistical empathetic energetic 
+    extroverted focused friendly giving happy hardworking helpful helpless honest 
+    idealistic impatient independent ingenious intelligent introverted irresponsible 
+    kind knowledgeable logical mature modest motivating needy nervous oblivious 
+    observant organized passive patient perserverant positive powerful proud reflective 
+    relaxed relentless responsible responsive rigid rude searching self-conscious selfish 
+    selfless sensible sentimental spontaneous strategic sympathetic tense trustworthy 
+    unavailable uncaring uncompromising unfocused unresponsive warm witty wise withholding)
     
     adjectives.each { |adj| Adjective.create!(name: adj)}
   end
