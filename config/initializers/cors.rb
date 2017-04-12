a05d72d9b934e85c8607ca06c1b6f600a9c3ae17
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-04-12 10:17:07 -0600

    Scope cors
    Allow only localhost:3000 and our production heroku application.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://johariwindow.herokuapp.com', 'http://johariwindow.herokuapp.com'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
+
